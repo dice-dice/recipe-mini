1501c21649dc2234a22c759d229ad369fb6f7a59
disuke <z7daisuke.debzzha@gmail.com>
2025-11-26 22:09:46 +0900

    feat: set up react-router-dom@6 and Styled-components@6(Global) in App.tsx

---
+++ concept/pagenation.md
@@ -0,0 +1,18 @@
+## 非ログイン
+- home /
+- 概要ページ　/overview
+- 登録ページ /resistration
+## ログイン
+- ログインページ /login
+- レシピ一覧ページ　app/recipes
+- レシピ詳細ページ app/recipes/:recipeId
+
+## choose library
+- react-hook-form@7 
+- react-router-dom@6
+- styled-components
+- hot-Toaster
+- react-icons
+
+## what create project
+- vite@4
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import Registration from "./pages/Registration";
+import Recipes from "./pages/Recipes";
+import RecipeDetail from "./pages/RecipeDetail";
+
 export default function App() {
   return (
-    <div>
-      <p>hello portfolio</p>
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="registration" element={<Registration />} />
+          <Route index element={<Navigate replace to="/" />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route path="recipes" element={<Recipes />}>
+              <Route path=":id" element={<RecipeDetail />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
-
