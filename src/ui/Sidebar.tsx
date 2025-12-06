c424daf8f7fd6c014d442fbbb1cd98e3c8ee666a
disuke <z7daisuke.debzzha@gmail.com>
2025-12-06 15:07:26 +0900

    fix: NavLink func in Sidebar

--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const StyleSidebar = styled.aside`
@@ -11,11 +12,28 @@ const StyleSidebar = styled.aside`
   gap: 3.2rem;
 `;
 
+const StyledNavLink = styled(NavLink)`
+  color: var(--color-grey-700);
+  text-decoration: none;
+  padding: 1rem;
+  border-radius: 5px;
+
+  &.active {
+    background-color: var(--color-brand-100);
+    color: var(--color-brand-800);
+    font-weight: bold;
+  }
+
+  &:hover {
+    background-color: var(--color-grey-100);
+  }
+`;
 
 export default function Sidebar() {
   return (
     <StyleSidebar >
-      
+      <StyledNavLink to={"/app/recipes"}>recipes</StyledNavLink>
+      <StyledNavLink to={"/app/add"}>recipe追加</StyledNavLink>
     </StyleSidebar>
   )
 }
