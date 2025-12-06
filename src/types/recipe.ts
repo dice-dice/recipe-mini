edf2d40d7ae861bcb8a670c895c9cdefe72cacd0
disuke <z7daisuke.debzzha@gmail.com>
2025-12-06 15:21:27 +0900

    feat: add AddRecipeForm type in recipe.ts

---
+++ src/types/recipe.ts
@@ -0,0 +1,17 @@
+export type Recipe = {
+  id: number;
+  created_at: string;
+  user_id: string;
+  title: string | null;
+  ingredients: string | null;
+  step1: string | null;
+  step2: string | null;
+  step3: string | null;
+  image_url: string | null;
+  category: string | null;
+};
+
+export type AddRecipeForm = Omit<
+  Recipe,
+  "id" | "created_at" | "user_id"
+>;
