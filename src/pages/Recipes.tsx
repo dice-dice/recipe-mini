09de77a503b0fc1da071a039c5f27407e4da7dfc
disuke <z7daisuke.debzzha@gmail.com>
2025-12-06 12:56:16 +0900

    feat: set up getAllRecipes in recipe.ts and integrate Recipes

--- src/pages/Recipes.tsx
+++ src/pages/Recipes.tsx
@@ -1,7 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { getAllRecipes } from "../services/recipe";
+import RecipeRow from "../features/recipes/RecipeRow";
+import { Recipe } from "../types/recipe";
+import { useEffect } from "react";
+import { supabase } from "../services/supabase";
+
 export default function Recipes() {
+  useEffect(() => {
+  supabase.auth.getUser().then((res) => {
+    console.log("ログイン中のユーザーID:", res.data.user?.id);
+  });
+}, []);
+
+  const {
+    isLoading,
+    data: recipes,
+    error,
+  } = useQuery<Recipe[]>({ queryKey: ["recipes"], queryFn: getAllRecipes });
   return (
-    <p>
-      recipesの画面
-    </p>
-  )
+    <ul>
+      {recipes?.map((recipe: Recipe) => (
+        <RecipeRow key={recipe.id} recipe={recipe} />
+      ))}
+    </ul>
+  );
 }
