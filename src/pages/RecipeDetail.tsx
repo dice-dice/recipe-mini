7fe0bc1d94079830705f89c914c5017276c9d455
disuke <z7daisuke.debzzha@gmail.com>
2025-12-09 07:25:59 +0900

    fix: add ConfirmPopUp and deleteRecipe

--- src/pages/RecipeDetail.tsx
+++ src/pages/RecipeDetail.tsx
@@ -1,15 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
-import { getRecipeById } from "../services/recipe";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteRecipe, getRecipeById } from "../services/recipe";
 import { Recipe } from "../types/recipe";
 import Spinner from "../ui/Spinner";
 import Button from "../ui/Button";
 import { useState } from "react";
 import EditRecipeForm from "../features/recipes/EditRecipeForm";
+import ConfirmPopUp from "../ui/ConfirmPopUp";
 
 export default function RecipeDetail() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
   const { id } = useParams();
   const [isEditing, setEditing] = useState(false);
+  const [showDeletePopup, setShowDeletePopup] = useState(false);
+
   const numericId = Number(id);
   const {
     isLoading,
@@ -20,31 +26,54 @@ export default function RecipeDetail() {
     queryFn: () => getRecipeById(numericId!),
     enabled: !!numericId,
   });
+
+  const { mutate: deleteByid } = useMutation({
+    mutationFn: () => deleteRecipe(numericId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["recipe"] });
+      navigate("/app/recipes");
+    },
+  });
+  function handleDelete() {
+    deleteByid();
+  }
   if (isLoading) return <Spinner />;
   if (error) return <p>データの取得に失敗しました。</p>;
   return (
     <>
-      <div>
-        <p>{recipe?.title}</p>
-        <img
-          src={recipe?.image_url ?? "/placeholder.png"}
-          alt={recipe?.title ?? "no title"}
-        />
-        <p>{recipe?.category}</p>
-        <h2>材料</h2>
-        <p>{recipe?.ingredients}</p>
+      <p>{recipe?.title}</p>
+      <img
+        src={recipe?.image_url ?? "/placeholder.png"}
+        alt={recipe?.title ?? "no title"}
+      />
+      <p>{recipe?.category}</p>
+      <h2>材料</h2>
+      <p>{recipe?.ingredients}</p>
 
-        <h2>手順</h2>
-        <p>{recipe?.step1}</p>
-        <p>{recipe?.step2}</p>
-        <p>{recipe?.step3}</p>
-        <Button size="small" onClick={() => setEditing(true)}>
-          編集
-        </Button>
-      </div>
+      <h2>手順</h2>
+      <p>{recipe?.step1}</p>
+      <p>{recipe?.step2}</p>
+      <p>{recipe?.step3}</p>
+      <Button size="small" onClick={() => setEditing(true)}>
+        編集
+      </Button>
+      <Button
+        size="small"
+        variation="danger"
+        onClick={() => setShowDeletePopup(true)}
+      >
+        削除
+      </Button>
+
       {isEditing && (
         <EditRecipeForm recipe={recipe!} onCancel={() => setEditing(false)} />
       )}
+      <ConfirmPopUp
+        open={showDeletePopup}
+        message="本当に削除しますか？"
+        onConfirm={handleDelete}
+        onClose={() => setShowDeletePopup(false)}
+      />
     </>
   );
 }
