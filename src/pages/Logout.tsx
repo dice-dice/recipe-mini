b8b763d92e0eaa9d2d1c093e23678a851841d7d4
disuke <z7daisuke.debzzha@gmail.com>
2025-12-09 07:21:52 +0900

    fix: change popup logic to ConfirmPopup

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,33 +1,14 @@
-import { useState } from "react";
+
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
-
-import Button from "../ui/Button";
 import { logout } from "../services/auth";
+import Button from "../ui/Button";
+import ConfirmPopUp from "../ui/ConfirmPopUp";
+import { useState } from "react";
 
-const PopupOverlay = styled.div`
-  position: fixed;
-  inset: 0;
-  background: rgba(0, 0, 0, 0.4);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const PopupBox = styled.div`
-  background: var(--color-grey-0);
-  padding: 2.4rem 3rem;
-  border-radius: var(--border-radius-md);
-  box-shadow: var(--shadow-lg);
-  display: flex;
-  flex-direction: column;
-  gap: 1.6rem;
-  text-align: center;
-`;
-
-export default function Logout() {
-  const [showPopup, setShowPopup] = useState(false);
+export default function Logout({ button_comment}:{button_comment:string }) {
+  
   const navigate = useNavigate();
+const [showPopup, setShowPopup] = useState(false);
 
   function handleSubmit() {
     logout();
@@ -36,31 +17,15 @@ export default function Logout() {
 
   return (
     <>
+    
       <Button
         onClick={() => setShowPopup(true)}
         size="small"
         variation="danger"
       >
-        Logout
+        {button_comment}
       </Button>
-
-      {showPopup && (
-        <PopupOverlay>
-          <PopupBox>
-            <p>LogOutしますか？</p>
-            <Button size="medium" variation="primary" onClick={handleSubmit}>
-              はい
-            </Button>
-            <Button
-              size="medium"
-              variation="primary"
-              onClick={() => setShowPopup(false)}
-            >
-              いいえ
-            </Button>
-          </PopupBox>
-        </PopupOverlay>
-      )}
-    </>
+      <ConfirmPopUp open={showPopup} onConfirm={handleSubmit} onClose={() => setShowPopup} message="logoutしますか？"/>
+      </>
   );
 }
