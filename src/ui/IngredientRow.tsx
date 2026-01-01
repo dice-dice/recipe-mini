4026bc6b3b5404e2cb5c2fd3ec9f8f70f75262c9
disuke <z7daisuke.debzzha@gmail.com>
2026-01-01 10:09:03 +0900

    feat: add IngredientRow layout component for ingredient form rows

---
+++ src/ui/IngredientRow.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const IndredientRow = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr 1fr auto;
+  gap: 0.8rem;
+  align-items: center;
+`;
+
+export default IndredientRow;
