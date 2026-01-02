5b0d377d90601c8a1fc80ef6434d64335148b57b
disuke <z7daisuke.debzzha@gmail.com>
2026-01-02 13:45:18 +0900

    feat: display dynamic page title in header

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -10,15 +10,22 @@ const StyledHeader = styled.header`
   border-bottom: 1px solid var(--color-grey-100);
 `;
 
+const HeaderTitle = styled.h2`
+  margin-right: auto;
+`
+
 export default function Header({
   theme,
   toggleTheme,
+  headerTitle
 }: {
   theme: string;
   toggleTheme: () => void;
+  headerTitle: string;
 }) {
   return (
     <StyledHeader>
+      <HeaderTitle>{headerTitle}</HeaderTitle>
       <Button onClick={toggleTheme} variation="secondary">
         {theme === "light" ? "🌙 Dark" : "☀️ Light"}
       </Button>
