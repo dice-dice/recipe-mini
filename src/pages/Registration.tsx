55b9017e080e515f2ee1222412f962bd5b9d8a1d
disuke <z7daisuke.debzzha@gmail.com>
2026-01-02 13:36:52 +0900

    refactor: introduce AuthLayout for authentication pages
    - Login.tsx
    - Registration.tsx

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,12 +1,10 @@
 import SignupForm from "../features/authentication/SignupForm";
-import Heading from "../ui/Heading";
-
+import AuthLayout from "../ui/AuthLayout";
 
 export default function Registration() {
   return (
-    <>
-     <Heading as="h1">新規登録フォーム</Heading> 
-     <SignupForm />
-    </>
-  )
+    <AuthLayout>
+      <SignupForm />
+    </AuthLayout>
+  );
 }
