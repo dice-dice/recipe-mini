5473bb8df128aaac6181c8db1bb8cefe1b701830
disuke <z7daisuke.debzzha@gmail.com>
2026-01-02 13:45:47 +0900

    style: center align heading components

--- src/ui/Heading.tsx
+++ src/ui/Heading.tsx
@@ -6,18 +6,21 @@ const Heading = styled.h1`
     css`
         font-size: 3rem;
         font-weight: 600;
+        text-align: center;
     `}
     ${(props) => 
     props.as === "h2" && 
     css`
         font-size: 2rem;
         font-weight: 600;
+        text-align: center;
     `}
     ${(props) => 
     props.as === "h3" && 
     css`
         font-size: 2rem;
         font-weight: 500;
+        text-align: center;
     `}
     ${(props) => 
     props.as === "h4" && 
